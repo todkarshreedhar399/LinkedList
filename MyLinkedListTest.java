8571a9f31a759fbb136239378405e549d4e5fddf
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-29 23:16:40 +0530

    Success : Appending Elements in Simple Linked List

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -17,4 +17,20 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
     }
+    @Test
+
+    public void given3NumbersWhenAppendedToLinkedListShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
     }
+}
